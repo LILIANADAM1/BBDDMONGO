ea7f2ee084018cd3aad94b60106d2c10927e9c1a
LILIANADAM1 <MMORENO.LILIANA@GMAIL.COM>
2024-05-03 10:35:23 +0200

    s

--- api-routes.js
+++ api-routes.js
@@ -9,4 +9,7 @@ router.get('/series', contentController.getAllSeries);
 router.get('/genre/:genre', contentController.getByGenre);
 router.get('/top10/:contentType', contentController.getTop10ByRating);
 
+router.post('/movies', contentController.createMovie);
+router.post('/series', contentController.createMovie);
+
 module.exports = router;
