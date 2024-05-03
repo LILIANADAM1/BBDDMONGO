e5094be29aa9fdec6958b29d2d4c5f235ac441a8
LILIANADAM1 <MMORENO.LILIANA@GMAIL.COM>
2024-05-03 09:59:03 +0200

    cvb

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const apiRoutes = require('./api-routes');
 
 const app = express();
-const port = process.env.PORT || 27017;
+const port = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
