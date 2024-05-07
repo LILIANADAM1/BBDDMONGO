c9bfccd8c1f31fc8c4f970b518400d77efc57d1b
LILIANADAM1 <MMORENO.LILIANA@GMAIL.COM>
2024-05-07 13:15:17 +0200

    dfs

--- contenidosModel.js
+++ contenidosModel.js
@@ -1,54 +1,29 @@
-//contenidosModel.js
-
-var mongoose = require('mongoose')
-
-//setup Schema 
-
-var valoracionesSchema = mongoose.Schema({
-    Nick: String,
-    Puntuacion: Number,
-    Comentario: String
-});
-
-var capituloSchema = mongoose.Schema({
-    Titulo: String,
-    Duracion: String,
-    Descripcion: String
-});
-
-
 var contenidoSchema = mongoose.Schema({
-    Titulo: {
-        type: String,
-        required: true
-    },
-    TipoContenido: {
-        type : String,
-        required: true
-    },
-    Descripcion: String,
-    Valoraciones: [
-        valoracionesSchema
-    ],
-    Generos: [String],
-    NumeroReproducciones: Number,
-    Premios: [String],
-    Pelicula: {
+  Titulo: {
+      type: String,
+      required: true
+  },
+  TipoContenido: {
+      type: String,
+      required: true
+  },
+  Descripcion: String,
+  Valoraciones: [valoracionesSchema],
+  Generos: [String],
+  NumeroReproducciones: Number,
+  Premios: [String],
+  Pelicula: {
       Duracion: String,
       Director: String
-    },
-    Serie: {
-      Temporadas: [
-        {
+  },
+  Serie: {
+      Temporadas: [{
           Numero: Number,
-          Capitulos: [
-            capituloSchema
-          ]
-        }
-      ]
-    }
-  }, {collection: 'Contenidos'});
-  
+          Capitulos: [capituloSchema]
+      }]
+  }
+}, { collection: 'Contenidos' });
+
 
 var Contenidos = module.exports = mongoose.model('Contenidos', contenidoSchema);
 
